09e682291e91abbe4a2ec20defdceabc6be38895
Rob <betoarpi@gmail.com>
2021-02-03 13:51:17 -0600

    Added emojis to score to enhance the user experience

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Emoji from 'react-native-emoji';
 
 const Score = ({ results, theme }) => {
+    const score = results/10;
+
+    const handleEmoji = () => {
+        return score <= 0.5 ?
+            'confused'
+        : score <= 0.7 ?
+            'flushed'
+        : score <= 0.9 ?
+            'grin'
+        : 
+            'sunglasses'
+    }
+
+    const handleMessage = () => {
+        return score <= 0.5 ?
+            'You can do it better'
+        : score <= 0.7 ?
+            'Oh, nice try'
+        : score <= 0.9 ?
+            'Wow! very nice'
+        :
+            'You are the best!'
+    }
+
     return (
         <View style={styles.container}>
-            <View style={
-                [
-                    styles.circle,
-                    { backgroundColor: theme.colors.button }
+            <View style={ [ styles.circle,
+                    { 
+                        backgroundColor: theme.colors.button
+                    }
             ]}>
                 <Text
-                    style={[
-                        styles.circleText, 
+                    style={[ styles.circleText, 
                         {
                             color: theme.colors.primary,
                         }
@@ -19,7 +43,23 @@ const Score = ({ results, theme }) => {
                     {`${results}/10`}
                 </Text>
             </View>
-            <Text style={[styles.text, {color: theme.colors.primary }]}>Your score!</Text>
+            <View style={styles.textEmoji}>
+                <Text
+                    style={[ styles.text,
+                        {
+                            color: theme.colors.primary
+                        }
+                ]}>
+                    {handleMessage()}
+                </Text>
+                <Emoji
+                    name={handleEmoji()}
+                    style={{
+                        fontSize: 24,
+                        marginLeft: 5
+                    }}
+                />
+            </View>
         </View>
     );
 };
@@ -44,6 +84,10 @@ const styles = StyleSheet.create({
     text: {
         fontFamily: 'SourceSansPro_700Bold',
         fontSize: 20
+    },
+    textEmoji: {
+        alignItems: 'center',
+        flexDirection: 'row'
     }
 });
 
